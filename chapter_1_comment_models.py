d9bce1d3724f922b1e1bb41fed361a1fe00f7eec
Rohit <rohitsingh200705@gmail.com>
2025-08-05 23:27:59 +0530

    Add files via upload

---
+++ chapter_1_comment_models.py
@@ -0,0 +1,15 @@
+import pyjokes  # pip install pyjokes
+
+# A python module is like a toolbox filled with special tools------(Single-line comment)------
+print("Printing jokes....")
+joke = pyjokes.get_joke()
+print(joke)
+"""
+so thanks 
+that was my program-------(Multiline comment)-----------
+another line 
+yet another line 
+
+ """
+"""  We can use python as a calculator by typing “python” + ⏎ on the terminal.
+ This opens REPL or Read Evaluate Print Loop."""
---
+++ chapter_1_practice_set.py
@@ -0,0 +1,54 @@
+
+# Question (1)-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+print(
+    """Twinkle, twinkle, little star,
+How I wonder what you are!
+Up above the world so high,
+Like a diamond in the sky.
+
+When the blazing sun is gone,
+When he nothing shines upon,
+Then you show your little light,
+Twinkle, twinkle, all the night.
+
+Then the traveler in the dark
+Thanks you for your tiny spark,
+How could he see where to go,
+If you did not twinkle so?
+
+In the dark blue sky you keep,
+Often through my curtains peep
+For you never shut your eye,
+Till the sun is in the sky.
+
+As your bright and tiny spark
+Lights the traveler in the dark,
+Though I know not what you are,
+Twinkle, twinkle, little star."""
+)
+
+# Question (2) : (photo-1)-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+# Question (3)-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+import pyttsx3  # pip install pyttsx3
+
+pyttsx3.speak("Hey I am good ")
+
+# Question (4)-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+import os
+
+directory_path = "/"
+contents = os.listdir(directory_path)
+print(item)
+
+# Question (5)-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+import os
+
+# Select the directory whose content you want to list
+directory_path = "/"
+
+# Use  the os module to list the directory content
+contents = os.listdir(directory_path)
+
+# Print the contents of the directory
+print(item)
